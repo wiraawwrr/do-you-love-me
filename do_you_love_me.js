298c8d801e065f371c0a909f8d8d8819d71b13bb
wiraawwrr <daichini71@gmail.com>
2025-10-24 21:51:49 +0700

    Update do_you_love_me.js

--- do_you_love_me.js
+++ do_you_love_me.js
@@ -10,4 +10,10 @@ yesBtn.addEventListener("click", () => {
   resultContainer.style.display = "flex";
 });
 
-noBtn.addEventListener("click", () => {});
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
